8472872ab4534aef998047545374209d04d0793f
viny3143 <155551924+viny3143@users.noreply.github.com>
2024-02-18 21:35:12 +0530

    Time: 7 ms (86.66%) | Memory: 12.4 MB (46.87%) - LeetSync

---
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    int mostWordsFound(vector<string>& sentences)
+    {
+        int n = sentences.size();
+        int maxi = 0;       
+        for(int i=0;i<n;i++)
+        {
+            int count = 1;
+            for(int j=0;j<sentences[i].size();j++)
+            {
+                if(sentences[i][j] == ' ')count++;
+            }
+            maxi = max(maxi,count);
+        }
+        return maxi;
+    }
+};
