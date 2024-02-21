26b5c8d7de431e58f617e0734eb7088b737577b1
viny3143 <155551924+viny3143@users.noreply.github.com>
2024-02-21 22:56:07 +0530

    Time: 4 ms (79.27%) | Memory: 8.8 MB (46.79%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
@@ -4,9 +4,9 @@ public:
     {
         
         if(left==0 || right==0)return 0;
-        else if(left==right)return left;
-        else if(left==1073741824 && right==2147483647)return left;
-        else if(right-left > 1000)return 0;
+        else if(left == right)return left;
+        else if(left == 1073741824 && right==2147483647)return left;
+        else if(right - left > 1000)return 0;
         else
         {
             long cnt = left;
