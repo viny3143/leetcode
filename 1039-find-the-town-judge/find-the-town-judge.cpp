650821df59d8f956bba08b18412d9b55e9add840
viny3143 <155551924+viny3143@users.noreply.github.com>
2024-02-22 21:27:24 +0530

    Time: 132 ms (70.19%) | Memory: 65.1 MB (56.29%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int findJudge(int n, vector<vector<int>>& trust)
+    {
+        set<int>st;
+        for(int i=0; i<trust.size(); i++)st.insert(trust[i][0]);
+        int cnt=0;
+        for(auto i:st)cnt += i;
+        int sum = n*(n+1)/2;
+        if(sum == cnt)return -1;
+        int cnt_1 = 0;
+        int val=sum-cnt;
+        for(int i=0; i<trust.size(); i++)
+        {
+                if(val == trust[i][1])cnt_1++;
+        }
+        if(cnt_1 == n-1)return val;
+        return -1;
+    }
+};
