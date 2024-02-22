bb262f2f1dcefaad74dad6c297e09dadff2d9369
viny3143 <155551924+viny3143@users.noreply.github.com>
2024-02-22 21:54:10 +0530

    Time: 4 ms (65.43%) | Memory: 13.5 MB (15.81%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
@@ -7,7 +7,6 @@ public:
         string str2;
         for(auto i : word1)str1 += i;
         for(auto i : word2)str2 += i;
-        if(str1 == str2)return true;
-        return false;
+        return (str1 == str2);
     }
 };
