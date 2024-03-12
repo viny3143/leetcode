9fb5a530392b700b128a601f5f91f5eaf9f7013a
viny3143 <155551924+viny3143@users.noreply.github.com>
2024-03-12 23:24:12 +0530

    Time: 8 ms (58.31%) | Memory: 15 MB (16.76%) - LeetSync

--- 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.cpp
+++ 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -11,33 +11,24 @@
 class Solution {
 public:
     ListNode* removeZeroSumSublists(ListNode* head) {
-        if (!head)
-            return nullptr;
+        unordered_map<int, ListNode*> prefixSum;
 
         ListNode* dummy = new ListNode(0);
         dummy->next = head;
 
-        ListNode* prefix = dummy;
-        unordered_map<int, ListNode*> prefixSum;
-
         int sum = 0;
-        while (prefix) {
+        for (ListNode* prefix = dummy; prefix != nullptr; prefix = prefix->next) {
             sum += prefix->val;
-            if (prefixSum.find(sum) != prefixSum.end()) {
-                ListNode* start = prefixSum[sum]->next;
-                int tempSum = sum + start->val;
-                while (tempSum != sum) {
-                    prefixSum.erase(tempSum);
-                    start = start->next;
-                    tempSum += start->val;
-                }
-                prefixSum[sum]->next = prefix->next;
-            } else {
-                prefixSum[sum] = prefix;
-            }
-            prefix = prefix->next;
+            prefixSum[sum] = prefix;
+        }
+
+        sum = 0;
+        for (ListNode* prefix = dummy; prefix != nullptr; prefix = prefix->next) {
+            sum += prefix->val;
+            prefix->next = prefixSum[sum]->next;
         }
 
         return dummy->next;
     }
 };
+
