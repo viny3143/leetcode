8df482460939490e84695a05fbf30ca14e446c33
viny3143 <155551924+viny3143@users.noreply.github.com>
2024-04-01 19:00:38 +0530

    Time: 4 ms (72.48%) | Memory: 12.2 MB (27.22%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -2,29 +2,26 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2)
     {
-        vector<int> vec;
-        for(int i : nums1)
+        vector<int>vec;
+        unordered_map<int, int> nextGreater;
+        stack<int> st;
+        for (int num : nums2)
         {
-            int cnt = 0;
-            for(int j = 0; j < nums2.size() - 1; j++)
+            while (!st.empty() && st.top() < num)
             {
-                if(nums2[j] == i)
-                {
-                    while(j + 1 < nums2.size()) // Corrected the loop condition
-                    {
-                        if(nums2[j + 1] > i) // Removed semicolon at the end of the if statement
-                        {
-                            cnt++;
-                            vec.push_back(nums2[j + 1]);
-                            break;
-                        }
-                        j++;
-                    }
-                }
+                nextGreater[st.top()] = num;
+                st.pop();
             }
-            if(cnt == 0)
-                vec.push_back(-1);
+            st.push(num);
         }
+            for (int num : nums1) {
+            if (nextGreater.find(num) != nextGreater.end()) {
+                vec.push_back(nextGreater[num]);
+            } else {
+                vec.push_back(-1); // Next greater element not found
+            }
+        }
+        
         return vec;
     }
 };
