b2ea12156d1c0f981f5dba3a95af4a5f9f21d3d3
viny3143 <155551924+viny3143@users.noreply.github.com>
2024-06-14 14:07:37 +0530

    Time: 637 ms (63.74%) | Memory: 30.2 MB (89.64%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.py
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.py
@@ -4,7 +4,7 @@ class Solution:
         cnt = 0
         for i in range(1, len(nums)):
             if nums[i] <= nums[i - 1]:
-                increment_needed = nums[i - 1] - nums[i] + 1
-                nums[i] += increment_needed
-                cnt += increment_needed
+                num = nums[i - 1] - nums[i] + 1
+                nums[i] += num
+                cnt += num
         return cnt
