51211ac21e1d56eb07f93a15f22c4db9f15b040c
viny3143 <155551924+viny3143@users.noreply.github.com>
2024-08-29 21:46:31 +0530

    Time: 36 ms (66.92%) | Memory: 35.6 MB (37.52%) - LeetSync

---
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    ListNode* insertGreatestCommonDivisors(ListNode* head) {
+        ListNode* temp = head;
+
+        while (temp != nullptr && temp->next != nullptr) {
+            int first_num = temp->val;
+            int second_num = temp->next->val;
+            int num = gcd(first_num, second_num);
+
+            ListNode* newNode = new ListNode(num);
+            newNode->next = temp->next;
+            temp->next = newNode;
+            temp = newNode->next; 
+        }
+
+        return head;
+    }
+};
