39d262ece796dc385ac275fe59164fad444558a6
viny3143 <155551924+viny3143@users.noreply.github.com>
2024-08-30 20:12:12 +0530

    Time: 428 ms (15.85%) | Memory: 185.1 MB (60.85%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
@@ -1,28 +1,19 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
-        vector<int>vec;
-        while(head != nullptr) {
-            vec.push_back(head->val);
-            head = head->next;
+        ListNode* first = head;
+        ListNode* second = head;
+        ListNode* kth = nullptr;
+        for (int i = 1; i < k; i++) {
+            first = first->next;
         }
-         swap(vec[k - 1], vec[vec.size() - k]);
-        ListNode* result = new ListNode(vec[0]);
-        ListNode* temp = result;
-        for(int i = 1; i< vec.size(); i++) {
-            temp->next = new ListNode(vec[i]);
-            temp = temp->next;
+        kth = first;
+        first = first->next;
+        while (first != nullptr) {
+            first = first->next;
+            second = second->next;
         }
-        return result;
+        swap(kth->val, second->val);
+        return head;
     }
 };
