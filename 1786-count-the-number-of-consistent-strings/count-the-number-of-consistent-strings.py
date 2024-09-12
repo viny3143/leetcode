4f997cbfc730aa367e7ddf4509072147adc8fcc9
viny3143 <155551924+viny3143@users.noreply.github.com>
2024-09-12 11:11:20 +0530

    Time: 236 ms (21.16%) | Memory: 18.4 MB (83.05%) - LeetSync

--- 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.py
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.py
@@ -3,6 +3,7 @@ class Solution:
         cnt = 0
         for i in words:
             for_check = 0
+            i = set(i)
             for check in i:
                 if check not in allowed:break
                 else:for_check += 1
