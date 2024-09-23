a50585d3d74ef565e5714d3bc73b2cecba9fb898
viny3143 <155551924+viny3143@users.noreply.github.com>
2024-09-23 18:51:52 +0530

    Time: 36 ms (59.05%) | Memory: 16.5 MB (43.23%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,10 +1,8 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        INT_MAX = 2**31 - 1
-        INT_MIN = -2**31
         check = str(x)
         if check[0] != '-':check = check[::-1]
         else:check = '-' + check[:0:-1]
         number = int(check)
-        if number > INT_MAX or number < INT_MIN:return 0
+        if number > 2**31 - 1 or number < -2**31:return 0
         return number
