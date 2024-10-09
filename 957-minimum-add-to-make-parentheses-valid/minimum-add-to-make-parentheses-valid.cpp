af1d2ff185709f963ddb0fe89c0412264410773d
viny3143 <155551924+viny3143@users.noreply.github.com>
2024-10-09 21:23:30 +0530

    Time: 4 ms (18.73%) | Memory: 7.9 MB (55.34%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char>st;
+        for(auto i:s) {
+            if(!st.empty() && i == ')' && st.top() == '(')st.pop();
+            else st.push(i);
+        }
+        return st.size();
+    }
+};
