704b24a05d3314696a8ec93382a2953031c9014d
viny3143 <155551924+viny3143@users.noreply.github.com>
2024-10-18 22:51:35 +0530

    Time: 2 ms (98.09%) | Memory: 8.5 MB (26.09%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         if (n <= 0) return false;
-        if (n == 1) return true;
-        if (n % 3 != 0) return false;
-        return isPowerOfThree(n / 3);
+        while (n % 3 == 0) {
+            n /= 3; 
+        }
+    return n == 1;
     }
 };
