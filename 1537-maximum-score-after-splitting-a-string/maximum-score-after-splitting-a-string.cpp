80e4844348de7a5076905832ec57421261aa61ab
viny3143 <155551924+viny3143@users.noreply.github.com>
2025-01-01 20:48:23 +0530

    Time: 5 ms (7.74%) | Memory: 8.9 MB (18.99%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -3,19 +3,18 @@ public:
     int maxScore(string s) {
         int check = 0;
         for(int i=1; i<s.size(); i++) {
-            int zero_cnt = 0;
-            int one_cnt = 0;
+            int count = 0;
             for(int zero = 0 ; zero < i ;zero++) {
                 if(s[zero] == '0') {
-                    zero_cnt++;
+                    count++;
                 }
             }
             for(int one = i ;one<s.size() ; one++) {
                 if(s[one] == '1') {
-                    one_cnt++;
+                    count++;
                 }
-                if( check <= zero_cnt + one_cnt) {
-                    check = zero_cnt + one_cnt;
+                if( check <= count) {
+                    check = count;
                 }
             }
 
